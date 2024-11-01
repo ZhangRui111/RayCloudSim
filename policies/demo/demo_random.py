eb3c6c888d059506d74217bf2699137004b18d39
recommender <85893320+diyicheng@users.noreply.github.com>
2024-11-02 02:08:19 +0800

    Delete policies directory

--- policies/demo/demo_random.py
+++
@@ -1,9 +0,0 @@
-from ..base_policy import BasePolicy
-
-import random
-
-
-class DemoRandom(BasePolicy):
-
-    def act(self, env, task):
-        return random.randint(0, len(env.scenario.get_nodes()) - 1)
